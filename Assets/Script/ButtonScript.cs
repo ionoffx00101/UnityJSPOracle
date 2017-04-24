383aa4574c77ae5ae0e90f1499f8673dc6f7f241
Nora <anes344@naver.com>
2017-04-24 19:03:16 +0900

    17.04.24
    첫 커밋
    유니티 <-> 웹서버(jsp) <-> DB(oracle) 구현

---
+++ Assets/Script/ButtonScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour {
+
+    // public ButtonScript _input;
+    // public ButtonScript _output;
+    public Text _output;
+
+
+    // http://192.168.1.27:8500/UforiaWS/scoreInput.jsp
+    // http://192.168.1.27:8500/UforiaWS/scoreOutput.jsp
+
+    // private string inputScoreUrl = "";
+    private string selectScoreUrl = "http://192.168.1.27:8500/UforiaWS/scoreOutput.jsp";
+
+    // public void inputScoreButton (){}
+
+    public void ReciveButton ()
+    {
+        StartCoroutine("ViewData");
+    }
+
+    IEnumerator ViewData ()
+    {
+        WWW result = DBConn.instance.GET(selectScoreUrl);
+
+        yield return result;
+
+        if ( result.isDone )
+        {
+            JSONNode jsonData = JSON.Parse(result.text);
+
+            Debug.Log(jsonData["scoreList"].Count);
+            Debug.Log(jsonData["scoreList"][1]["score_name"].Value);
+
+            _output.text = "";
+            for(int i = 0 ; i < jsonData["scoreList"].Count ;i++ )
+            {
+                _output.text += jsonData["scoreList"][i]["score_name"].Value + "\n";
+                _output.text += jsonData["scoreList"][i]["score_score"].Value + "\n";
+            }
+        }
+    }
+}
---
+++ Assets/Script/DBConn.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class DBConn : MonoBehaviour
+{
+    private static DBConn _instance;
+    public static DBConn instance { get { return _instance; } }
+    private void Awake ()
+    {
+        _instance = this;
+    }
+
+    public WWW GET (String url)
+    {
+        WWW www = new WWW(url);
+        StartCoroutine(WaitForRequest(www));
+
+        return www;
+    }
+
+    public WWW POST(String url,Dictionary<string,string> post)
+    {
+        WWWForm form = new WWWForm();
+        foreach(KeyValuePair<String,String> post_arg in post )
+        {
+            form.AddField(post_arg.Key , post_arg.Value);
+        }
+        WWW www = new WWW(url , form);
+
+        StartCoroutine(WaitForRequest(www));
+        return www;
+    }
+
+    private IEnumerator WaitForRequest (WWW www)
+    {
+        yield return www;
+
+        if ( www.error == null )
+        {
+            Debug.Log("응.." + www.text);
+        }
+        else
+        {
+            Debug.Log("응.. 에러" + www.error);
+        }
+    }
+}
